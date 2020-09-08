b4fa2299c0e708a036619b7057cc9786dc764313
Ichiro Nakamura <windpublic@gmail.com>
2020-09-09 01:01:32 +0900

    足し算と引き算を追加

--- 9cc.c
+++ 9cc.c
@@ -2,15 +2,35 @@
 #include  <stdlib.h>
 
 int main(int argc, char **argv){
-  if(argc != 2){
+  if(argc!=2){
     fprintf(stderr, "引数の個数が正しくありません\n");
     return 1;
   }
-
+  char *p = argv[1];
   printf(".intel_syntax noprefix\n");
   printf(".globl main\n");
   printf("main:\n");
-  printf("  mov rax, %d\n", atoi(argv[1]));
+  /* strtol 関数は数値を読み込んだ後、第2引数のポインタを
+   * アップデートして、読み込んだ最後の文字の次の文字を指すように
+   * 値を更新する */
+  printf("  mov rax, %ld\n", strtol(p,&p,10));
+
+  while(*p){
+    if(*p == '+'){
+      p++;
+      printf("  add rax, %ld\n", strtol(p,&p,10));
+      continue;
+    }
+    if(*p == '-'){
+      p++;
+      printf("  sub rax, %ld\n", strtol(p,&p,10));
+      continue;
+    }
+
+    fprintf(stderr, "予期しない文字です: '%c'\n", *p);
+    return 1;
+  }
+
   printf("  ret\n");
   return 0;
 }
